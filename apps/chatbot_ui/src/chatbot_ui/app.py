fb4da3aedfeeafcfcfc213089e407d84534ccfd7
Aditya Natani <adi4exp@gmail.com>
2026-01-18 23:01:48 +0530

    Implement RAG System
    Implemented foundational RAG System
    Embeded LangSmith
    Crated Evaluation Dataset

--- apps/chatbot_ui/src/chatbot_ui/app.py
+++ apps/chatbot_ui/src/chatbot_ui/app.py
@@ -34,19 +34,6 @@ def api_call(method, url, **kwargs):
         return False, {"message": str(e)}
 
 
-with st.sidebar:
-    st.title("Settings")
-    provider = st.selectbox("Select Provider", ["OpenAI", "Groq", "Google"])
-    if provider == "OpenAI":
-        model_name = st.selectbox("Select Model", ["gpt-5-mini", "gpt-5-nano"])
-    elif provider == "Groq":
-        model_name = st.selectbox("Select Model", ["llama-3.3-70b-versatile"])
-    elif provider == "Google":
-        model_name = st.selectbox("Select Model", ["gemini-2.5-flash"])
-
-    st.session_state.provider = provider
-    st.session_state.model_name = model_name
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages.append({"role": "assistant", "content": "Hello! How can I assist today?."})
@@ -61,9 +48,9 @@ if prompt := st.chat_input("Hello! How can I assist today?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        output = api_call("post", f"{config.API_URL}/chat", json={"provider": st.session_state.provider, "model_name": st.session_state.model_name, "messages": st.session_state.messages})
+        output = api_call("post", f"{config.API_URL}/rag", json={"query": prompt})
         response_data = output[1]
-        answer = response_data["message"]
+        answer = response_data["answer"]
         st.write(answer)
     st.session_state.messages.append({"role": "assistant", "content": answer})
     
