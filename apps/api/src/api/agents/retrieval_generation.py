540b5cbfb635c17a047e01428958f04b888649a1
adityanatani <adityanatani@users.noreply.github.com>
2026-01-26 02:38:12 +0530

    Prompt Registry Implementation

--- apps/api/src/api/agents/retrieval_generation.py
+++ apps/api/src/api/agents/retrieval_generation.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel,Field
 import instructor
 import numpy as np
 from qdrant_client.models import FieldCondition, Filter, MatchValue, Prefetch, FusionQuery, Document
+from api.agents.utils.prompt_management import prompt_template_config
 
 class RAGUsedContext(BaseModel):
     id:str = Field(description="The ID of the items used to answer the question")
@@ -102,30 +103,9 @@ def process_context(context):
 )
 def build_prompt(preprocessed_context, question):
 
-    prompt = f"""
-You are a shopping assistant that can answer questions about the products in stock.
-
-You will be given a question and a list of context.
-
-Instructtions:
-- You need to answer the question based on the provided context only.
-- Never use word context and refer to it as the available products.
-- As an output you need to provide:
-
-* The answer to the question based on the provided context.
-* The list of the IDs of the chunks that were used to answer the question. Only return the ones that are used in the answer.
-* Short description (1-2 sentences) of the item based on the description provided in the context.
-
-- The short description should have the name of the item.
-- The answer to the question should contain detailed information about the product and returned with detailed specification in bullet points.
-
-Context:
-{preprocessed_context}
-
-Question:
-{question}
-"""
-
+    template = prompt_template_config("api/agents/prompts/retrieval_generation.yaml", "retrieval_generation")
+    prompt = template.render(preprocessed_context=preprocessed_context, question=question)
+    
     return prompt
 
 @traceable(
